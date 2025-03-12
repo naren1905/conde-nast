347004a6d793f3f205bffa082ce2ffad5842c29b
GOVINDARAJULU Narendhar Kumar <narendhar.kumarg@al-enterprise.com>
2025-03-12 14:50:19 +0530

    App for Conde-nast

--- src/index.js
+++ src/index.js
@@ -4,28 +4,36 @@ const client = require('prom-client');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Metrics Registry
 const register = new client.Registry();
 client.collectDefaultMetrics({ register });
 
-// Request Counter
+
 const httpRequestCounter = new client.Counter({
   name: 'http_requests_total',
   help: 'Total number of HTTP requests',
+  labelNames: ['method', 'route', 'status']
 });
 register.registerMetric(httpRequestCounter);
 
-// Main Route
+
+app.use((req, res, next) => {
+  res.on('finish', () => {
+    httpRequestCounter.labels(req.method, req.path, res.statusCode).inc();
+  });
+  next();
+});
+
 app.get('/', (req, res) => {
-  httpRequestCounter.inc(); // Count requests
   res.send('Hello World');
 });
 
-// Metrics Route
 app.get('/metrics', async (req, res) => {
   res.set('Content-Type', register.contentType);
   res.end(await register.metrics());
 });
 
-// Start Server
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+  console.log(`Metrics available at http://localhost:${PORT}/metrics`);
+});
