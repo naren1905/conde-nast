c72760ee1c62ff5765b3b4fc2975f881272e9d0d
GOVINDARAJULU Narendhar Kumar <narendhar.kumarg@al-enterprise.com>
2025-03-13 10:41:58 +0530

    Hello world and test suits

--- src/index.js
+++ src/index.js
@@ -7,14 +7,12 @@ const PORT = process.env.PORT || 3000;
 const register = new client.Registry();
 client.collectDefaultMetrics({ register });
 
-
 const httpRequestCounter = new client.Counter({
   name: 'http_requests_total',
   help: 'Total number of HTTP requests',
   labelNames: ['method', 'route', 'status']
 });
 register.registerMetric(httpRequestCounter);
-
 
 app.use((req, res, next) => {
   res.on('finish', () => {
@@ -32,8 +30,11 @@ app.get('/metrics', async (req, res) => {
   res.end(await register.metrics());
 });
 
+if (require.main === module) {
+  app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+    console.log(`Metrics available at http://localhost:${PORT}/metrics`);
+  });
+}
 
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
-  console.log(`Metrics available at http://localhost:${PORT}/metrics`);
-});
+module.exports = app;
--- tests/app.test.js
+++ tests/app.test.js
@@ -1,9 +1,19 @@
 const request = require('supertest');
-const app = require('../src/index'); // Import the Express app
+const app = require('../src/index.js'); 
+
+let server;
+
+beforeAll(() => {
+  server = app.listen(3000); 
+});
+
+afterAll((done) => {
+  server.close(done); 
+});
 
 describe('GET /', () => {
   it('should return Hello World', async () => {
-    const res = await request(app).get('/');
+    const res = await request(server).get('/');
     expect(res.statusCode).toBe(200);
     expect(res.text).toBe('Hello World');
   });
